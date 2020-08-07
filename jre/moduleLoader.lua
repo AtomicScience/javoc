1f22d590ea481f84d0b9526beb929038df28f349
AtomicScience <dmitry.greblyuk@mail.ru>
2020-08-07 14:11:34 +0300

    ModuleLoader modified to support module caching

--- jre/moduleLoader.lua
+++ jre/moduleLoader.lua
@@ -5,15 +5,27 @@
 
 local moduleLoader = {}
 
+-- Cache for loaded modules.
+moduleLoader.loaded = {}
+
 ---Loads a module from "jre" directory
----@param path string     @ Path to the module to load
----                         Paths should be relative to the 'jre'
----                         library (e.g. "class/classLoader")
----@return table module	  @ Loaded module
-function moduleLoader.require(path)
+---@param path string              @ Path to the module to load
+---                                  Paths should be relative to the 'jre'
+---                                  library (e.g. "class/classLoader")
+---@param reloadFromFile boolean   @ Set to true if module should be forced to
+---                                  be reloaded from the file
+---@return table module	           @ Loaded module
+function moduleLoader.require(path, ignoreCache)
+	-- If cache contains preloaded module and ignoreCache is false or nii,
+	-- returing the cached module
+	if moduleLoader.loaded[path] and not ignoreCache then
+		return moduleLoader.loaded[path]
+	end
+
 	local loadedModule = dofile("../jre/" .. path .. ".lua")
 
 	if loadedModule then
+		moduleLoader.loaded[path] = loadedModule
 		return loadedModule
 	else
 		error("Failed to load module!")
