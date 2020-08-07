e077faa026827c8b29649ab90c85a4b8a3d8989e
AtomicScience <dmitry.greblyuk@mail.ru>
2020-08-07 14:04:31 +0300

    ModuleLoader implemented

--- jre/moduleLoader.lua
+++ jre/moduleLoader.lua
@@ -8,10 +8,16 @@ local moduleLoader = {}
 ---Loads a module from "jre" directory
 ---@param path string     @ Path to the module to load
 ---                         Paths should be relative to the 'jre'
----                         library (e.g. "classLoader/classLoader")
+---                         library (e.g. "class/classLoader")
 ---@return table module	  @ Loaded module
 function moduleLoader.require(path)
+	local loadedModule = dofile("../jre/" .. path .. ".lua")
 
+	if loadedModule then
+		return loadedModule
+	else
+		error("Failed to load module!")
+	end
 end
 
 return moduleLoader
