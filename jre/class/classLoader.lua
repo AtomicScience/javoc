695c91bedaf171bc30906205a8a42db4042e5450
AtomicScience <dmitry.greblyuk@mail.ru>
2021-08-14 23:14:05 +0300

    "Imported" often-used modules in the class loader

--- jre/class/classLoader.lua
+++ jre/class/classLoader.lua
@@ -1,5 +1,7 @@
 local javoc, classLoader = require("umfal")("javoc")
 
+local debugPrint = javoc.jre.debug.print
+local binaryStream = javoc.jre.utilities.binaryStream
 ---Loads a class from a file. Path is relative from classpath
 ---@param file string           @ Name of the class to load
 ---@param classpath string      @ Absolute path to the classes to load
@@ -11,15 +13,15 @@ function classLoader.loadClassFromFile(file, classpath)
 
 	local filePath = "" .. classpath .. file
 
-	javoc.jre.debug.print("Attempting to load class on path of " .. filePath)
+	debugPrint("Attempting to load class on path of " .. filePath)
 	local stream = io.open(filePath, "rb")
 
 	if not stream then
-		javoc.jre.debug.print("Failed to find the file or open it")
+		debugPrint("Failed to find the file or open it")
 		error("Class File not found!")
 	end
 
-	javoc.jre.debug.print("File stream created, loading class from it")
+	debugPrint("File stream created, loading class from it")
 	return classLoader.loadClassFromStream(stream)
 end
 
@@ -33,50 +35,50 @@ function classLoader.loadClassFromStream(stream)
 		error("Class loading aborted: incorrect 'magic value'")
 	end
 
-	javoc.jre.debug.print("Correct 'magic value' is loaded")
+	debugPrint("Correct 'magic value' is loaded")
 
 	class.version = classLoader.loadVersion(stream)
 	class.constantPool = classLoader.loadConstantPool(stream)
 	class.accessFlags = classLoader.loadAccessFlags(stream)
 	class.thisClass, class.superClass = classLoader.loadClassNames(stream, class.constantPool)
 
-	javoc.jre.debug.print("Class " .. class.thisClass.name .. " loaded successfully")
+	debugPrint("Class " .. class.thisClass.name .. " loaded successfully")
 
 	return class
 end
 
 function classLoader.checkMagicValue(stream)
-	javoc.jre.debug.print("Loading 'magic value' (0xCAFEBABE)")
-	local magicValue = javoc.jre.utilities.binaryStream.readU4(stream)
+	debugPrint("Loading 'magic value' (0xCAFEBABE)")
+	local magicValue = binaryStream.readU4(stream)
 
 	return (magicValue == 0xCAFEBABE)
 end
 
 function classLoader.loadVersion(stream)
-	javoc.jre.debug.print("Loading versions (major and minor)")
+	debugPrint("Loading versions (major and minor)")
 	local version = {}
 
-	version.minor = javoc.jre.utilities.binaryStream.readU2(stream)
-	version.major = javoc.jre.utilities.binaryStream.readU2(stream)
+	version.minor = binaryStream.readU2(stream)
+	version.major = binaryStream.readU2(stream)
 
-	javoc.jre.debug.print("Versions loaded successfully!")
-	javoc.jre.debug.print("Major - " .. version.major .. "; Minor - " .. version.minor)
+	debugPrint("Versions loaded successfully!")
+	debugPrint("Major - " .. version.major .. "; Minor - " .. version.minor)
 	return version
 end
 
 function classLoader.loadConstantPool(stream)
-	javoc.jre.debug.print("Loading constant pool")
+	debugPrint("Loading constant pool")
 
 	local constantPool = {}
-	constantPool.size = javoc.jre.utilities.binaryStream.readU2(stream)
-	javoc.jre.debug.print("Size of the constant pool - " .. constantPool.size)
+	constantPool.size = binaryStream.readU2(stream)
+	debugPrint("Size of the constant pool - " .. constantPool.size)
 
 	local currentIndex = 1
 
-	javoc.jre.debug.print("Constants in the pool:")
+	debugPrint("Constants in the pool:")
 	while currentIndex < constantPool.size do
-		local tag = javoc.jre.utilities.binaryStream.readU1(stream)
-		javoc.jre.debug.print("Constant #" .. currentIndex .. " with tag " .. tag)
+		local tag = binaryStream.readU1(stream)
+		debugPrint("Constant #" .. currentIndex .. " with tag " .. tag)
 		-- Since Double and Long constants occuppy two indexes in the
 		-- constant pool, they will return not one constant, but two:
 		-- the actual one and the 'dummy'. "Normal" constants will
@@ -104,47 +106,47 @@ end
 
 -- TODO: REFACTOR THIS MESS ASAP!!!!
 function classLoader.loadAccessFlags(stream)
-	javoc.jre.debug.print("Loading access flags")
+	debugPrint("Loading access flags")
 
 	local accessFlags = {}
-	accessFlags.mask = javoc.jre.utilities.binaryStream.readU2(stream)
+	accessFlags.mask = binaryStream.readU2(stream)
 
-	javoc.jre.debug.print("Mask: " .. string.format("0x%x", accessFlags.mask))
+	debugPrint("Mask: " .. string.format("0x%x", accessFlags.mask))
 
-	accessFlags["ACC_PUBLIC"]     = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x0001)
-	javoc.jre.debug.print("ACC_PUBLIC: " .. tostring(accessFlags["ACC_PUBLIC"]))
+	accessFlags["ACC_PUBLIC"]     = binaryStream.mask(accessFlags.mask, 0x0001)
+	debugPrint("ACC_PUBLIC: " .. tostring(accessFlags["ACC_PUBLIC"]))
 
-	accessFlags["ACC_FINAL"]      = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x0010)
-	javoc.jre.debug.print("ACC_FINAL: " .. tostring(accessFlags["ACC_FINAL"]))
+	accessFlags["ACC_FINAL"]      = binaryStream.mask(accessFlags.mask, 0x0010)
+	debugPrint("ACC_FINAL: " .. tostring(accessFlags["ACC_FINAL"]))
 
-	accessFlags["ACC_SUPER"]      = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x0020)
-	javoc.jre.debug.print("ACC_SUPER: " .. tostring(accessFlags["ACC_SUPER"]))
+	accessFlags["ACC_SUPER"]      = binaryStream.mask(accessFlags.mask, 0x0020)
+	debugPrint("ACC_SUPER: " .. tostring(accessFlags["ACC_SUPER"]))
 
-	accessFlags["ACC_INTERFACE"]  = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x0200)
-	javoc.jre.debug.print("ACC_INTERFACE: " .. tostring(accessFlags["ACC_INTERFACE"]))
+	accessFlags["ACC_INTERFACE"]  = binaryStream.mask(accessFlags.mask, 0x0200)
+	debugPrint("ACC_INTERFACE: " .. tostring(accessFlags["ACC_INTERFACE"]))
 
-	accessFlags["ACC_ABSTRACT"]   = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x0400)
-	javoc.jre.debug.print("ACC_ABSTRACT: " .. tostring(accessFlags["ACC_ABSTRACT"]))
+	accessFlags["ACC_ABSTRACT"]   = binaryStream.mask(accessFlags.mask, 0x0400)
+	debugPrint("ACC_ABSTRACT: " .. tostring(accessFlags["ACC_ABSTRACT"]))
 
-	accessFlags["ACC_SYNTHETIC"]  = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x1000)
-	javoc.jre.debug.print("ACC_SYNTHETIC: " .. tostring(accessFlags["ACC_SYNTHETIC"]))
+	accessFlags["ACC_SYNTHETIC"]  = binaryStream.mask(accessFlags.mask, 0x1000)
+	debugPrint("ACC_SYNTHETIC: " .. tostring(accessFlags["ACC_SYNTHETIC"]))
 
-	accessFlags["ACC_ANNOTATION"] = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x2000)
-	javoc.jre.debug.print("ACC_ANNOTATION: " .. tostring(accessFlags["ACC_ANNOTATION"]))
+	accessFlags["ACC_ANNOTATION"] = binaryStream.mask(accessFlags.mask, 0x2000)
+	debugPrint("ACC_ANNOTATION: " .. tostring(accessFlags["ACC_ANNOTATION"]))
 
-	accessFlags["ACC_ENUM"]       = javoc.jre.utilities.binaryStream.mask(accessFlags.mask, 0x4000)
-	javoc.jre.debug.print("ACC_ENUM: " .. tostring(accessFlags["ACC_ENUM"]))
+	accessFlags["ACC_ENUM"]       = binaryStream.mask(accessFlags.mask, 0x4000)
+	debugPrint("ACC_ENUM: " .. tostring(accessFlags["ACC_ENUM"]))
 
 	return accessFlags
 end
 
 function classLoader.loadClassNames(stream, constantPool)
-	javoc.jre.debug.print("Loading this and super class names")
+	debugPrint("Loading this and super class names")
 	local thisClass  = {}
 	local superClass = {}
 
-	thisClass.index  = javoc.jre.utilities.binaryStream.readU2(stream)
-	superClass.index = javoc.jre.utilities.binaryStream.readU2(stream)
+	thisClass.index  = binaryStream.readU2(stream)
+	superClass.index = binaryStream.readU2(stream)
 
 	-- Both 'thisClassIndex' and 'superClassIndex' point at
 	-- the CONSTANT_Class, which points at the CONSTANT_Utf8,
@@ -158,8 +160,8 @@ function classLoader.loadClassNames(stream, constantPool)
 	thisClass.name  = thisClassUTF8.value
 	superClass.name = superClassUTF8.value
 
-	javoc.jre.debug.print("This class:  #" .. thisClass.index .. " (" .. thisClass.name .. ")")
-	javoc.jre.debug.print("Super class: #" .. superClass.index .. " (" .. superClass.name .. ")")
+	debugPrint("This class:  #" .. thisClass.index .. " (" .. thisClass.name .. ")")
+	debugPrint("Super class: #" .. superClass.index .. " (" .. superClass.name .. ")")
 	return thisClass, superClass
 end
 
