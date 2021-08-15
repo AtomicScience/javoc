a51353c145d05f5c634ce1d830d4b71c522d7459
AtomicScience <dmitry.greblyuk@mail.ru>
2021-08-15 23:48:13 +0300

    Excessive commentary removed

--- javoc/bin/javocp/javocp.lua
+++ javoc/bin/javocp/javocp.lua
@@ -5,12 +5,8 @@ local javoc         = require("umfal").initAppFromRelative("javoc", 3)
 -- The flag to indicate if output should be verbose or not
 local verbose = false
 
--- Parsing arguments passed to the program via "shell" API
--- Refer to (https://ocdoc.cil.li/api:shell) for more information
 local args, ops = shell.parse(...)
 
--- If special options are provided or no class is given,
--- we should print the help message
 if ops["?"] or ops["help"] or (#args == 0) then
 	javoc.bin.javocp.output.printHelpMessage()
 	return
@@ -24,7 +20,6 @@ if ops["d"] or ops["debug"] then
 	javoc.util.debug.debugEnabled = true
 end
 
--- Name of the class to load
 local classToLoad = args[1]
 
 local class = javoc.class.classLoader.loadClassFromFile(classToLoad, "")
--- javoc/bin/javocp/serialization/constantPool.lua
+++ javoc/bin/javocp/serialization/constantPool.lua
@@ -3,17 +3,6 @@ local javoc = require("umfal")("javoc")
 
 local serialization = {}
 
----Serializes a constant from the constant pool
----@param constantPool table   @ Table of the constant pool - output of the
----                              classLoader.loadConstantPool() function
----@param index number         @ Index of the constant pool entry you want to
----                              serialize
----
----
----@return string value        @ Value of the constant
----                              Nil, if constant is dummy
----@return string commentary   @ A commentary to the constant
----                              Nil if not needed or dummy
 function serialization.toString(constantPool, index)
 	local constant = constantPool[index]
 	local type = constant.type
