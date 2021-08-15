1b161cbad8f5ab5b10600414ee1b26010dad4b46
AtomicScience <dmitry.greblyuk@mail.ru>
2021-08-15 23:47:17 +0300

    Access flags serializer refactored

--- javoc/bin/javocp/serialization/accessFlags.lua
+++ javoc/bin/javocp/serialization/accessFlags.lua
@@ -1,55 +1,25 @@
-local accessFlags = {}
+local javoc, accessFlags = require("umfal")("javoc")
 
--- TODO: Refactor the mess below
----Compiles class access
----@param accessFlags table    @ Table of the access flags - output of the
----                              classLoader.loadAccessFlags() function
----@return string string       @ Access flags in human-readable form
----                              This form can't be unserialized
-function accessFlags.toString(accessFlags)
+local function accessFlagMaskAsString(accessFlagMask)
 	-- Extending the mask number to always have 4 digits
 	-- For example, 0x20 will be extended to 0x0020
-	local stringMask         = string.format("%x", accessFlags.mask)
+	local stringMask         = string.format("%x", accessFlagMask)
 	local stringMaskExtended = string.rep("0", 4 - string.len(stringMask)) .. stringMask
 
-	local line = "(0x" .. stringMaskExtended .. ")  "
+	return "(0x" .. stringMaskExtended .. ")  "
+end
 
-	if accessFlags["ACC_PUBLIC"] then
-		line = line .. "ACC_PUBLIC, "
-	end
+function accessFlags.toString(accessFlagsTable)
+	local line = accessFlagMaskAsString(accessFlagsTable.mask)
 
-	if accessFlags["ACC_FINAL"] then
-		line = line .. "ACC_FINAL, "
-	end
-
-	if accessFlags["ACC_SUPER"] then
-		line = line .. "ACC_SUPER, "
-	end
-
-	if accessFlags["ACC_INTERFACE"] then
-		line = line .. "ACC_INTERFACE, "
-	end
-
-	if accessFlags["ACC_ABSTRACT"] then
-		line = line .. "ACC_ABSTRACT, "
-	end
-
-	if accessFlags["ACC_SYNTHETIC"] then
-		line = line .. "ACC_SYNTHETIC, "
-	end
-
-	if accessFlags["ACC_ANNOTATION"] then
-		line = line .. "ACC_ANNOTATION, "
-	end
-
-	if accessFlags["ACC_ENUM"] then
-		line = line .. "ACC_ENUM, "
-	end
+	for flagName, _ in pairs(javoc.class.values.accessFlagsMasks) do
+		if accessFlagsTable[flagName] then
+			line = line .. flagName .. ", "
+		end
+    end
 
 	-- Removing last two chars on the string (comma and whitespace)
 	line = line:sub(1, -3)
 
 	return line
 end
-
-return accessFlags
